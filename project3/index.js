958a406a4c80d7dbb155e423f0d9e4bb5b511f82
Sergio Rocha <91704421+Cytaro@users.noreply.github.com>
2023-08-02 11:12:13 -0300

    Update index.js

--- project3/index.js
+++ project3/index.js
@@ -1,1 +1,39 @@
-
+const calculate = () => {
+    let port = document.querySelector("#port").value;
+    let hist = document.querySelector("#hist").value;
+    let mate = document.querySelector("#mate").value;
+    let fis = document.querySelector("#fis").value;
+    let nota = "";
+    let totalnotas =
+        parseFloat(port) +
+        parseFloat(hist) +
+        parseFloat(mate) +
+        parseFloat(fis);
+    let calculo = (totalnotas / 4);
+    if (calculo <= 10 && calculo >= 8) {
+        nota = "entre 10 e 8";
+    } else if (calculo <= 7 && calculo >= 6) {
+        nota = "entre 7 e 6";
+    } else if (calculo <= 5 && calculo >= 4) {
+        nota = "nota 5";
+    } else {
+        nota = "entre 5 e 4";
+    }
+    if (port == "" || hist == ""
+                || mate == "" || fis == "") {
+        document.querySelector("#showdata").innerHTML
+            = "Please enter all the fields";
+    } else {
+        if (calculo >= 5) {
+        document.querySelector(
+            "#showdata"
+        ).innerHTML =
+            ` Nota final é ${nota}. Voce passou. `;
+        } else {
+        document.querySelector(
+            "#showdata"
+        ).innerHTML =
+            ` Nota final é ${nota}. Voce Reprovou. `;
+        }
+    }
+};
